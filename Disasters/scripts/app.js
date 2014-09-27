669349ec6e8611ec3a03dacbc750d9d062188153
Slavi <slavicvetanov@gmail.com>
2014-09-27 11:41:29 +0300

    Slight change in models

--- Disasters/scripts/app.js
+++ Disasters/scripts/app.js
@@ -1,46 +1,11 @@
 (function (global) {
-
-    // store a reference to the application object that will be created
-    // later on so that we can use it if need be
     var app = global.app = global.app || {};
 
-    // create an object to store the models for each view
-    app.models = {
-        home: {
-            title: 'Home'
-        },
-        settings: {
-            title: 'Settings'
-        },
-        about: {
-            title: 'About'
-        },
-        map: {
-            title: 'Map',
-            initMap: function (e) {
-                var myOptions = {
-                    center: new google.maps.LatLng(-34.397, 150.644),
-                    zoom: 8,
-                    mapTypeId: google.maps.MapTypeId.ROADMAP
-                };
-
-                var mapElement = $("#map-canvas");
-                var container = e.view.content;
-
-                var map = new google.maps.Map(mapElement[0], myOptions);
-            }
-        },
-        alerts: {
-            title: 'Alerts'
-        },
-        addAlert: {
-            title: 'Add Alert'
-        },
-        alertInfo: {
-            title: 'Alert Info'
-        }
-    };
-    
+    // simple object
+    app.models = app.models || {};
+    app.models.home = { title: 'Home' };
+    app.models.settings = { title: 'Settings' };
+    app.models.about = { title: 'About'};
 
     document.addEventListener('deviceready', function () {
         navigator.splashscreen.hide();
@@ -51,6 +16,5 @@
         });
 
     }, false);
-
 
 }(window));
