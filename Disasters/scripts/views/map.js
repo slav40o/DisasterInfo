fb86a6adb25f55de9e66beff70e4375f7da08c77
Slavi <slavicvetanov@gmail.com>
2014-09-27 12:41:12 +0300

    json data added

--- Disasters/scripts/views/map.js
+++ Disasters/scripts/views/map.js
@@ -1,16 +1,39 @@
 /* window, kendo */
 
 (function (global) {
-    var app = global.app = global.app || {};
+    var app = global.app = global.app || {},
+        map,
+        geocoder,
+        MapViewModel;
 
-    var MapViewModel = kendo.data.ObservableObject.extend({
-        init: function () {
+    MapViewModel = kendo.data.ObservableObject.extend({
+        _markers: null,
+        _isLoading: false,
+        location: null,
+
+
+        showLoading: function () {
+            if (this._isLoading) {
+                app.kendoApp.showLoading();
+            }
+        },
+
+        hideLoading: function () {
+            app.kendoApp.hideLoading();
+        },
+
+        _putMarker: function (position) {
             var that = this;
 
-            kendo.data.ObservableObject.fn.init.apply(that, []);
+            if (that._lastMarker !== null && that._lastMarker !== undefined) {
+                that._lastMarker.setMap(null);
+            }
 
-            
-        },
+            that._lastMarker = new google.maps.Marker({
+                map: map,
+                position: position
+            });
+        }
     });
 
 })(window);
