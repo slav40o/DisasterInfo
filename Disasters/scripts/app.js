30ab9c198d063ba4104b84f439fca62fcdf3cb0a
Slavi <slavicvetanov@gmail.com>
2014-09-27 13:16:15 +0300

    added app.data and some initial map logic(not working)

--- Disasters/scripts/app.js
+++ Disasters/scripts/app.js
@@ -15,6 +15,15 @@
             initial: 'views/home.html'
         });
 
+        app.data = new kendo.data.DataSource({
+            transport: {
+                read: {
+                    url: "data/alerts.json",
+                    dataType: "json"
+                }
+            }
+        });
+
     }, false);
 
 }(window));
--- Disasters/scripts/views/map.js
+++ Disasters/scripts/views/map.js
@@ -3,14 +3,20 @@
 (function (global) {
     var app = global.app = global.app || {},
         map,
-        geocoder,
         MapViewModel;
 
     MapViewModel = kendo.data.ObservableObject.extend({
-        _markers: null,
         _isLoading: false,
         location: null,
+        markers: [],
 
+        loadMarkers: function(markersPositions){
+
+        },
+
+        navigateHome: function(){
+
+        },
 
         showLoading: function () {
             if (this._isLoading) {
@@ -25,15 +31,46 @@
         _putMarker: function (position) {
             var that = this;
 
-            if (that._lastMarker !== null && that._lastMarker !== undefined) {
-                that._lastMarker.setMap(null);
-            }
-
-            that._lastMarker = new google.maps.Marker({
+            that.markers.push(new google.maps.Marker({
                 map: map,
                 position: position
-            });
+            }));
         }
     });
 
+    app.locationService = {
+        initLocation: function () {
+            var mapOptions = {
+                zoom: 15,
+                mapTypeId: google.maps.MapTypeId.ROADMAP,
+                zoomControl: true,
+                zoomControlOptions: {
+                    position: google.maps.ControlPosition.LEFT_BOTTOM
+                },
+
+                mapTypeControl: false,
+                streetViewControl: false
+            };
+
+            map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+            app.locationService.viewModel.loadMarkers.apply(app.locationService.viewModel, []);
+            app.locationService.viewModel.onNavigateHome.apply(app.locationService.viewModel, []);
+        },
+
+        show: function () {
+            //show loading mask in case the location is not loaded yet 
+            //and the user returns to the same tab
+            app.locationService.viewModel.showLoading();
+
+            //resize the map in case the orientation has been changed while showing other tab
+            google.maps.event.trigger(map, "resize");
+        },
+
+        hide: function () {
+            //hide loading mask if user changed the tab as it is only relevant to location tab
+            app.locationService.viewModel.hideLoading();
+        },
+
+        viewModel: new LocationViewModel()
+    };
 })(window);
