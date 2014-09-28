3210cf5c81e0484a15bf73bd716fe33b4e727a12
Gabriela Angelova <gabriela.angelova@fdiba.tu-sofia.bg>
2014-09-28 23:55:47 +0300

    schema and fields for adding new disaster

--- Disasters/scripts/views/add-alert.js
+++ Disasters/scripts/views/add-alert.js
@@ -11,8 +11,20 @@
 
 
         },
+        addAlert: function () {
+            var disaster = {};
+            disaster.type = document.getElementById('type').value;
+            disaster.info = document.getElementById('info').value;
+            disaster.lat = document.getElementById('lat').value;
+            disaster.long = document.getElementById('long').value;
+            disaster.area = document.getElementById('area').value;
+            disaster.imagePath = document.getElementById('imagePath').value; //TO DO
+            disaster.isActive = document.getElementById('isActive').value;  // TO DO
+        },
     });
 
     //TO DO: Set the model view to the app obejct!!!
-
+    app.addDisaster = {
+        model: new AddAlertViewModel()
+    }
 })(window);
