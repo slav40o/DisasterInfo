a41cc712643cc25fdb8e2a74f559a0c6df840804
Gabriela Angelova <gabriela.angelova@fdiba.tu-sofia.bg>
2014-09-29 20:10:34 +0300

    added alerts' adding, not good visualisation of alert's  details

--- Disasters/scripts/views/add-alert.js
+++ Disasters/scripts/views/add-alert.js
@@ -9,7 +9,7 @@
 
             kendo.data.ObservableObject.fn.init.apply(that, []);
 
-
+            that.set("addAlertsDataSource", app.data);
         },
         addAlert: function () {
             var disaster = {};
@@ -18,8 +18,34 @@
             disaster.lat = document.getElementById('lat').value;
             disaster.long = document.getElementById('long').value;
             disaster.area = document.getElementById('area').value;
-            disaster.imagePath = document.getElementById('imagePath').value; //TO DO
-            disaster.isActive = document.getElementById('isActive').value;  // TO DO
+            disaster.imagePath = document.getElementById('imgPath').value;
+            disaster.isActive = document.getElementById('isActive').checked;
+            disaster.id = Math.floor(Math.random() * (100 - 8) + 8);
+
+            //app.data._data.push(JSON.stringify(disaster));
+            app.data.add(disaster);
+            //app.data.add({
+            //    id: disaster.id,
+            //    done: false,
+            //    description: disaster
+            //});
+
+            //works only in IE...
+            //var fso = new ActiveXObject("Scripting.FileSystemObject");
+            //var jsonFile = fso.getFile("data/alerts.json");
+            //jsonFile.WriteLine("This is a test");
+            //jsonFile.close();
+        },
+        showDetails: function (e) {
+            var that = new AddAlertViewModel();
+
+            var data = that.get("addAlertsDataSource");
+
+            app.data.fetch(function () {
+                var id = e.view.params.uid;
+                var model = data.at(parseInt(id) - 1);
+                kendo.bind(e.view.element, model, kendo.mobile.ui);
+            });
         },
     });
 
